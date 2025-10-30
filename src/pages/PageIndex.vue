58fe2971d1c3703cf5728c2df6a0fc7de9f09502
mmk31585 <mmk3151385@gmail.com>
2025-10-30 16:49:12 +0330

    feat: change home page + rename it +use basepanel

---
+++ src/pages/PageIndex.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { Search } from '@/components/widgets/search'
+</script>
+
+<template>
+  <main
+    class="tw:grid tw:grid-cols-12 tw:grid-rows-6 tw:gap-4 tw:p-4 tw:bg-base-background tw:w-screen tw:h-screen"
+  >
+    <section class="tw:col-span-9 tw:row-span-6 tw:bg-foreground tw:w-full tw:h-full"></section>
+    <section class="tw:flex tw:flex-col tw:col-span-3 tw:row-span-6 tw:gap-4 tw:w-full tw:h-full">
+      <Search class="tw:flex-1"></Search>
+      <div class="tw:flex-2 tw:bg-foreground"></div>
+    </section>
+  </main>
+</template>
