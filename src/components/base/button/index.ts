e3ac3c874bec31fd97a0cff4e72b4d08e6ede6e3
mmk31585 <mmk3151385@gmail.com>
2025-11-05 10:06:13 +0330

    feat: export Base btns

--- src/components/base/button/index.ts
+++ src/components/base/button/index.ts
@@ -1,1 +1,4 @@
 export * from './types.ts'
+export { default as BaseButton } from './BaseButton.vue'
+export { default as BaseGroupButton } from './BaseGroupButton.vue'
+export { default as BaseSpeedDialButton } from './BaseSpeedDialButton.vue'
