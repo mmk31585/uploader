74783b504701405afe3ee7f968890c70a30a35ee
mmk31585 <mmk3151385@gmail.com>
2025-11-05 10:04:26 +0330

    feat: remove defaults + tooltip

--- src/components/base/button/BaseSpeedDialButton.vue
+++ src/components/base/button/BaseSpeedDialButton.vue
@@ -2,57 +2,42 @@
 import SpeedDial from 'primevue/speeddial'
 import type { SpeedDialProps } from './types'
 import { computed } from 'vue'
-import type { MenuItem } from 'primevue/menuitem'
 import { cn } from '@/utils'
 
 const props = withDefaults(defineProps<SpeedDialProps>(), {
   direction: 'up',
   mask: false,
+  tooltipShow: true,
+  tooltipEvent: 'hover',
 })
-const defaultItems: MenuItem[] = [
-  {
-    label: 'Add',
-    icon: 'pi pi-pencil',
-    command: () => {
-      console.log('Add clicked')
-    },
-  },
-  {
-    label: 'Update',
-    icon: 'pi pi-refresh',
-    command: () => {
-      console.log('Update clicked')
-    },
-  },
-  {
-    label: 'Delete',
-    icon: 'pi pi-trash',
-    command: () => {
-      console.log('Delete clicked')
-    },
-  },
-  {
-    label: 'Upload',
-    icon: 'pi pi-upload',
-    command: () => {
-      console.log('Upload clicked')
-    },
-  },
-]
+
 const ptProp = computed(() => {
   const root = props.className?.root ?? undefined
-  // const iconCls = props.className?.icon ?? undefined;
 
   return {
     root: cn(root),
-    // icon: cn(iconCls)
   }
 })
 const model = computed(() => {
-  return props.items && props.items.length ? props.items : defaultItems
+  return props.items
+})
+
+const tpPosition = computed(() => {
+  if (!props.tooltipShow) return undefined
+  if (!props.tooltipPosition) return 'right'
+  return props.tooltipPosition
 })
 </script>
 
 <template>
-  <SpeedDial :model="model" :direction="direction" :mask="mask" :pt="ptProp" />
+  <SpeedDial
+    :model="model"
+    :direction="direction"
+    :mask="mask"
+    :pt="ptProp"
+    :tooltipOptions="{
+      event: tooltipEvent,
+      position: tpPosition,
+    }"
+  />
 </template>
