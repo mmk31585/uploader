0935dd428044dede1ebfd03f0125310f4bbfd444
mmk31585 <mmk3151385@gmail.com>
2025-11-05 10:09:07 +0330

    feat: change the name export

--- src/components/base/button/index.ts
+++ src/components/base/button/index.ts
@@ -1,4 +1,4 @@
 export * from './types.ts'
 export { default as BaseButton } from './BaseButton.vue'
 export { default as BaseGroupButton } from './BaseGroupButton.vue'
-export { default as BaseSpeedDialButton } from './BaseSpeedDialButton.vue'
+export { default as BaseFloatButton } from './BaseSpeedDialButton.vue'
