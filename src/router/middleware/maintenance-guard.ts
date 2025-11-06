a8c03d0c08134ca2fe3332cc41b13b21eb0f197b
mmk31585 <mmk3151385@gmail.com>
2025-11-06 16:55:06 +0330

    refactor: name

--- src/router/middleware/maintenance-guard.ts
+++ src/router/middleware/maintenance-guard.ts
@@ -2,14 +2,14 @@ import type { RouteLocationNormalized } from 'vue-router'
 import { useMaintenanceStore } from '@/stores'
 
 export function checkMaintenanceGuard(to: RouteLocationNormalized) {
-  const { isMaintenance } = useMaintenanceStore()
+  const { isMaintencenane } = useMaintenanceStore()
 
   // Prevent redirect loop
   if (to.path.startsWith('/maintenance')) return
 
   if (to.path.startsWith('/api') || to.path.startsWith('/_')) return
 
-  if (isMaintenance) {
+  if (isMaintencenane) {
     return { name: 'maintenance' }
   }
 
