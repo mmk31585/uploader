7899e62aad8e2b2abfb5f39fd5af4b895b5f7748
mmk31585 <mmk3151385@gmail.com>
2025-11-06 16:56:18 +0330

    feat: add the category component

--- src/pages/PageIndex.vue
+++ src/pages/PageIndex.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { Category } from '@/components/widgets/categories'
 import { Search } from '@/components/widgets/search'
 </script>
 
@@ -15,10 +16,10 @@ import { Search } from '@/components/widgets/search'
   <main
     class="tw:grid tw:grid-cols-12 tw:grid-rows-6 tw:gap-4 tw:p-4 tw:bg-base-background tw:w-screen tw:h-screen"
   >
-    <section class="tw:col-span-9 tw:row-span-6 tw:bg-foreground tw:w-full tw:h-full"></section>
     <section class="tw:flex tw:flex-col tw:col-span-3 tw:row-span-6 tw:gap-4 tw:w-full tw:h-full">
-      <Search class="tw:flex-1"></Search>
-      <div class="tw:flex-2 tw:bg-foreground"></div>
+      <Search class=""></Search>
+      <Category class="tw:h-full"></Category>
     </section>
+    <section class="tw:col-span-9 tw:row-span-6 tw:bg-foreground tw:w-full tw:h-full"></section>
   </main>
 </template>
